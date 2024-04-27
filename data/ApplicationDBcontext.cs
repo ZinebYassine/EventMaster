22841f9365522f356a84a460827cc79b85f5b5a0
zineb <zineby100@gmail.com>
2024-04-27 11:33:10 +0200

    first commit

---
+++ data/ApplicationDBcontext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.data
+{
+    public class ApplicationDBcontext : IdentityDbContext
+    {
+        public ApplicationDBcontext(DbContextOptions<ApplicationDBcontext> options) : base(options) { }
+
+
+
+    } }
