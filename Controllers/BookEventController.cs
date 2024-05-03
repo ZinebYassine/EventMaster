c0c45573b5f01a460fd2ddb5c0721e80558e714f
zineb <zineby100@gmail.com>
2024-05-03 22:45:42 +0200

    Controllers: BookEvent & Customer

--- Controllers/BookEventController.cs
+++ Controllers/BookEventController.cs
@@ -1,12 +1,81 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApp.data;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    public class BookEventController1 : Controller
+    [Authorize]
+    public class BookEventController : Controller
     {
+
+
+        public readonly ApplicationDBcontext _db;
+        public BookEventController(ApplicationDBcontext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var objBookEventList = _db.BookEvents.ToList();
+            return View(objBookEventList);
         }
+
+
+        public IActionResult Create()
+        {
+            // Fetch the list of available venues from the database
+            var availableVenues = _db.Venues.ToList();
+
+            // Create a new instance of the EventViewModel and set the AvailableVenues property
+            var model = new BookEvent
+            {
+                AvailableVenues = availableVenues.Select(v => new SelectListItem { Value = v.Id.ToString(), Text = v.Name }).ToList()
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(BookEvent model)
+        {
+            // Check if the model is valid before processing
+            if (!ModelState.IsValid)
+            {
+                // If not valid, fetch the list of available venues again and return the view with errors
+                model.AvailableVenues = _db.Venues.Select(v => new SelectListItem { Value = v.Id.ToString(), Text = v.Name }).ToList();
+                return View(model);
+            }
+
+            // Convert the view model to the entity models (BookEvent and Event) and save to the database
+            var newBookEvent = new BookEvent
+            {
+                Name = model.Name,
+                date = model.date,
+                Venue = model.Venue
+            };
+
+            var newEvent = new Event
+            {
+                Name = model.Name,
+                date = model.date,
+                Venue = model.Venue
+                // Add other properties as needed
+            };
+
+            _db.BookEvents.Add(newBookEvent);
+            _db.Events.Add(newEvent);
+
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
     }
 }
+
--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,12 +1,93 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.data;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    public class CustomerController1 : Controller
+    [Authorize]
+    public class CustomerController : Controller
     {
+
+        public readonly ApplicationDBcontext _db;
+        public CustomerController(ApplicationDBcontext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            var objCustomerList = _db.Customers.ToList();
+            return View(objCustomerList);
+        }
+
+        //Get
+
+        public IActionResult Create()
         {
             return View();
         }
+
+        //Post
+        [HttpPost]
+        public IActionResult Create(Customer Cust)
+        {
+            _db.Customers.Add(Cust);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            //return Cust;
+        }
+        public IActionResult Delete(int id)
+        {
+            Customer custToDelete = _db.Customers.Find(id);
+
+            if (custToDelete == null)
+            {
+                // Customer not found, handle appropriately (e.g., return a not found view)
+                return NotFound();
+            }
+
+            // Delete the customer
+            _db.Customers.Remove(custToDelete);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Customer custToEdit = _db.Customers.Find(id);
+
+            if (custToEdit == null)
+            {
+
+                return NotFound();
+            }
+
+            return View(custToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer updatedCust)
+        {
+
+            Customer existingCust = _db.Customers.Find(updatedCust.Id);
+
+            if (existingCust == null)
+            {
+                // Customer not found, handle appropriately (e.g., return a not found view)
+                return NotFound();
+            }
+            existingCust.FirstName = updatedCust.FirstName;
+            existingCust.LastName = updatedCust.LastName;
+            existingCust.Phone = updatedCust.Phone;
+
+
+
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
+
