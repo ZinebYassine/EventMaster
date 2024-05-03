d8012d8144db7adf07da3dd82eba7d36b4cafd5c
zineb <zineby100@gmail.com>
2024-05-03 22:22:20 +0200

    update customer/venue

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,6 +1,15 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApp.Models
 {
     public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Phone { get; set; }
+
     }
 }
+
--- Models/Venue.cs
+++ Models/Venue.cs
@@ -1,6 +1,15 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApp.Models
 {
     public class Venue
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Manager { get; set; }
+        public int ManagerPhone { get; set; }
+        public int Capacity { get; set; }
+        public string? adresse { get; set; }
     }
 }
