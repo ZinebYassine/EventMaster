933af052da350da5d0e22a8c37a128bfc0cf50aa
zineb <zineby100@gmail.com>
2024-05-03 22:07:14 +0200

    Models/views/controllers

---
+++ Controllers/VenueController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class VenueController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
