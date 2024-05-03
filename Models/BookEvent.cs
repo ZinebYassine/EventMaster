caab42355b44321454fcd429bbd9b264cab0e676
ayabelaoui <ayabelaoui1@gmail.com>
2024-05-03 22:18:40 +0200

    update event/bookevent

--- Models/BookEvent.cs
+++ Models/BookEvent.cs
@@ -1,6 +1,19 @@
-﻿namespace WebApp.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
 {
     public class BookEvent
     {
-    }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? date { get; set; }
+        public string? Venue { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem> AvailableVenues { get; set; }
+    }
 }
--- Models/Event.cs
+++ Models/Event.cs
@@ -1,6 +1,21 @@
-﻿namespace WebApp.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
 {
     public class Event
     {
-    }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? date { get; set; }
+        public string? Venue { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem> AvailableVenues { get; set; }
+
+    }
 }
